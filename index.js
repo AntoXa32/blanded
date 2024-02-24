0564ab1c1f20036cc96eca4276845fa05cef55e9
Serhii Orlenko <sorlenko@grifano.com>
2024-02-24 14:32:44 +0100

    add time format task

--- index.js
+++ index.js
@@ -7,13 +7,33 @@
  *? інакше відобразити:"Не знаєте? ECMAScript!"
  */
 
-function askCwastion() {
-  const userAnswe = prompt("Яка офіційна назва JavaScript?");
-  if (userAnswe === "ECMAScript") {
-    alert("Вірно!");
-  } else {
-    alert("Не знаєте? ECMAScript!");
-  }
+// function askCwastion() {
+//   const userAnswe = prompt("Яка офіційна назва JavaScript?");
+//   if (userAnswe === "ECMAScript") {
+//     alert("Вірно!");
+//   } else {
+//     alert("Не знаєте? ECMAScript!");
+//   }
+// }
+
+// askCwastion();
+
+/**
+Напишіть програму, яка отримає від користувача
+ число (кількість хвилин) та виведе в консоль
+ рядок у форматі годин та хвилин
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ 70 === 01:10
+ */
+
+function getTime(minuts) {
+  const hours = Math.floor(minuts / 60);
+  const hoursFormat = String(hours).padStart(2, 0);
+  const minutes = String(minuts % 60).padStart(2, 0);
+
+  return `${hoursFormat}:${minutes}`;
 }
 
-askCwastion();
+const number = prompt("Add number");
+alert("Your time is: " + getTime(number));
+// console.log(getTime(number));
