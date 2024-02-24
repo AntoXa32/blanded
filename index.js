22f72b1085df7aea41e72b21aa2b664d58ce5c83
yarman777 <152394200+yarman777@users.noreply.github.com>
2024-02-24 16:12:21 +0200

    add age

--- index.js
+++ index.js
@@ -26,14 +26,32 @@
  70 === 01:10
  */
 
-function getTime(minuts) {
-  const hours = Math.floor(minuts / 60);
-  const hoursFormat = String(hours).padStart(2, 0);
-  const minutes = String(minuts % 60).padStart(2, 0);
+// function getTime(minuts) {
+//   const hours = Math.floor(minuts / 60);
+//   const hoursFormat = String(hours).padStart(2, 0);
+//   const minutes = String(minuts % 60).padStart(2, 0);
 
-  return `${hoursFormat}:${minutes}`;
-}
+//   return `${hoursFormat}:${minutes}`;
+// }
 
-const number = prompt("Add number");
-alert("Your time is: " + getTime(number));
+// const number = prompt("Add number");
+// alert("Your time is: " + getTime(number));
 // console.log(getTime(number));
+
+// Створіть змінну з ім'ям "userAge" і присвойте їй значення вашого віку. Напишіть функцію з ім'ям "checkAge",
+//   яка приймає один параметр "вік" і перевіряє, чи є цей вік дорослим(більше або дорівнює 18).Функція повинна
+//   повертати true, якщо вік дорослий, і false в іншому випадку.Викличте цю функцію, передаючи змінну "userAge",
+//   і виведіть результат в консоль.
+
+const userAge = 35;
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+function checkAge(age) {
+  return age >= 18;
+}
+console.log("🚀 ~ checkAge:", checkAge(userAge));
